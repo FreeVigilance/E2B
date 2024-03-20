ed9f62bfe4a45f9d2eb14aa2861002ecd5ad7c77
nscheremitsin <nscheremitsin@edu.hse.ru>
2024-03-20 15:54:27 +0300

    fix enums by ch str behavior

--- backend/backend/app/src/shared/enums.py
+++ backend/backend/app/src/shared/enums.py
@@ -1,9 +1,7 @@
 import enum
 
-from extensions.enums import BaseIntEnum, BaseStrEnum
 
-
-class NullFlavor(BaseStrEnum):
+class NullFlavor(enum.StrEnum):
     @staticmethod
     def _generate_next_value_(member_name: str, *_) -> str:
         return member_name
@@ -18,24 +16,24 @@ class NullFlavor(BaseStrEnum):
     PINF = enum.auto()  # Positive Infinity
 
 
-class C_1_3_type_report(BaseIntEnum):
+class C_1_3_type_report(enum.IntEnum):
     SPONTANEOUS_REPORT = 1
     REPORT_FROM_STUDY = 2
     OTHER = 3
     NOT_AVAILABLE_TO_SENDER = 4
 
 
-class C_1_8_2_first_sender(BaseIntEnum):
+class C_1_8_2_first_sender(enum.IntEnum):
     REGULATOR = 1
     OTHER = 2
 
 
-class C_1_11_1_report_nullification_amendment(BaseIntEnum):
+class C_1_11_1_report_nullification_amendment(enum.IntEnum):
     NULLIFICATION = 1
     AMENDMENT = 2
 
 
-class C_2_r_4_qualification(BaseIntEnum):
+class C_2_r_4_qualification(enum.IntEnum):
     PHYSICIAN = 1
     PHARMACIST = 2
     OTHER_HEALTH_PROFESSIONAL = 3
@@ -43,11 +41,11 @@ class C_2_r_4_qualification(BaseIntEnum):
     CONSUMER_OR_OTHER_NON_HEALTH_PROFESSIONAL = 5
 
 
-class C_2_r_5_primary_source_regulatory_purposes(BaseIntEnum):
+class C_2_r_5_primary_source_regulatory_purposes(enum.IntEnum):
     PRIMARY = 1
 
 
-class C_3_1_sender_type(BaseIntEnum):
+class C_3_1_sender_type(enum.IntEnum):
     PHARMACEUTICAL_COMPANY = 1
     REGULATORY_AUTHORITY = 2
     HEALTH_PROFESSIONAL = 3
@@ -57,13 +55,13 @@ class C_3_1_sender_type(BaseIntEnum):
     PATIENT_OR_CONSUMER = 7
 
 
-class C_5_4_study_type_reaction(BaseIntEnum):
+class C_5_4_study_type_reaction(enum.IntEnum):
     CLINICAL_TRIALS = 1
     INDIVIDUAL_PATIENT_USE = 2
     OTHER_STUDIES = 3
 
 
-class D_2_3_patient_age_group(BaseIntEnum):
+class D_2_3_patient_age_group(enum.IntEnum):
     FOETUS = 0
     NEONATE = 1
     INFANT = 2
@@ -73,24 +71,24 @@ class D_2_3_patient_age_group(BaseIntEnum):
     ELDERLY = 6
 
 
-class D_5_sex(BaseIntEnum):
+class D_5_sex(enum.IntEnum):
     MALE = 1
     FEMALE = 2
 
 
-class D_10_6_sex_parent(BaseIntEnum):
+class D_10_6_sex_parent(enum.IntEnum):
     MALE = 1
     FEMALE = 2
 
 
-class E_i_3_1_term_highlighted_reporter(BaseIntEnum):
+class E_i_3_1_term_highlighted_reporter(enum.IntEnum):
     YES_NOT_SERIOUS = 1
     NO_NOT_SERIOUS = 2
     YES_SERIOUS = 3
     NO_SERIOUS = 4
 
 
-class E_i_7_outcome_reaction_last_observation(BaseIntEnum):
+class E_i_7_outcome_reaction_last_observation(enum.IntEnum):
     UNKNOWN = 0
     RECOVERED_OR_RESOLVED = 1
     RECOVERING_OR_RESOLVING = 2
@@ -99,21 +97,21 @@ class E_i_7_outcome_reaction_last_observation(BaseIntEnum):
     FATAL = 5
 
 
-class F_r_3_1_test_result_code(BaseIntEnum):
+class F_r_3_1_test_result_code(enum.IntEnum):
     POSITIVE = 1
     NEGATIVE = 2
     BORDERLINE = 3
     INCONCLUSIVE = 4
 
 
-class G_k_1_characterisation_drug_role(BaseIntEnum):
+class G_k_1_characterisation_drug_role(enum.IntEnum):
     SUSPECT = 1
     CONCOMITANT = 2
     INTERACTING = 3
     DRUG_NOT_ADMINISTERED = 4
 
 
-class G_k_8_action_taken_drug(BaseIntEnum):
+class G_k_8_action_taken_drug(enum.IntEnum):
     UNKNOWN = 0
     DRUG_WITHDRAWN = 1
     DOSE_REDUCED = 2
@@ -122,14 +120,14 @@ class G_k_8_action_taken_drug(BaseIntEnum):
     NOT_APPLICABLE = 9
 
 
-class G_k_9_i_4_reaction_recur_readministration(BaseIntEnum):
+class G_k_9_i_4_reaction_recur_readministration(enum.IntEnum):
     YES_YES = 1
     YES_NO = 2
     YES_UNK = 3
     NO_NA = 4
 
 
-class G_k_10_r_additional_information_drug(BaseIntEnum):
+class G_k_10_r_additional_information_drug(enum.IntEnum):
     COUNTERFEIT = 1
     OVERDOSE = 2
     DRUG_TAKEN_BY_THE_FATHER = 3
